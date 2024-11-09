94c62a4780b760c11590cb1da0910dc2b74f758e
mariamksalama <mariam_salama97@hotmail.com>
2024-11-09 21:03:07 +0200

    adding lottie anumation

--- client/src/Components/Weather/weather.tsx
+++ client/src/Components/Weather/weather.tsx
@@ -2,35 +2,49 @@ import { Box } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { fetchWeather } from '../Weather/weatherUtil';
 import Search from '../Search/search';
+import LottieWeatherAnimation from './lottieWeatherAnimation';
 
 
 const Weather: React.FC = () => {
   const [weather, setWeather] = useState<string | null>(null);
-  const [cityName, setCityName] = useState<string | null>(null);
-
+  const [isLoading , setIsLoading] = useState<boolean>(true);
+    const [cityName, setCityName] = useState<string | null>(null);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       async position => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-         fetchWeather({latitude, longitude}).then((weather) => {
-          if(weather){
-            setWeather(weather);
-          }
-        });
+        Promise.all([
+            fetchWeather({ latitude, longitude }),
+            new Promise(resolve => setTimeout(resolve, 2000))
+          ]).then(([weather]) => {
+            if (weather) {
+              setWeather(weather);
+            } else {
+              setWeather(null);
+            }
+            setIsLoading(false);
+          });
         
       });
   }, []);
     
   const handleSearchSubmit = (cityName: string) => {
+    setIsLoading(true);
     setCityName(cityName);
-    fetchWeather({cityName}).then((weather) => {
-        if(weather){
-            console.log(weather);
+    Promise.all([
+        fetchWeather({ cityName }),
+        new Promise(resolve => setTimeout(resolve, 2000))
+      ]).then(([weather]) => {
+        if (weather) {
           setWeather(weather);
+        } else {
+          setWeather(null);
         }
+        setIsLoading(false);
       });
+   
   };
 
   
@@ -38,8 +52,31 @@ const Weather: React.FC = () => {
 
   return (
     <Box>
+        
+        {isLoading? (<Box
+          sx={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1000,
+          }}
+        >
+          <LottieWeatherAnimation />
+        </Box>):
+        (
+            <>
       <Search cityName={cityName} onSubmit={handleSearchSubmit}/>
       {weather && <Box>{weather}</Box>}
+        </>
+            )}
+    
+        
     </Box>
   );
 };
